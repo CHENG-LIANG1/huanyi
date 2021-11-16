2c6fbd964609d246f5ce0255d3f2bb47e7d2bd6a
Cheng Liang <madfoxfoxy@gmail.com>
2021-11-16 11:09:29 +0800

    Added nav and background color

---
+++ src/components/Nav/navbar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { ReactDOM } from "react";
+import { Nav, NavLink, Container, Navbar } from 'react-bootstrap';
+import "./nav.css"
+
+function NavigationBar(){
+
+  return(<div className="navi">
+    <Navbar collapseOnSelect expand="lg"  variant="dark">
+      <Navbar.Brand className="logo" href="#home"> 
+        Huanyi
+      </Navbar.Brand>
+      <Navbar.Toggle className="ham" aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="links">
+          <Nav.Link className="link" bg="dark" href="#features">首页</Nav.Link>
+          <Nav.Link className="link" href="#pricing">简介</Nav.Link>
+          <Nav.Link className="link" href="#pricing">简介</Nav.Link>
+          <Nav.Link className="link" href="#pricing">简介</Nav.Link>
+          <Nav.Link className="link" href="#pricing">简介</Nav.Link>
+        </Nav>
+
+      </Navbar.Collapse>
+    </Navbar>
+    
+    </div>)
+}
+
+export default NavigationBar
