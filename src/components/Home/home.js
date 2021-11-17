f70c8de2e83b2e7d20a82cdf21cb0e3e8c734bfe
Cheng Liang <madfoxfoxy@gmail.com>
2021-11-17 15:50:43 +0800

    update

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -9,66 +9,57 @@ function Home(){
             <Container fluid className="cards">
                 <Row>
                     <Col>
-                        <Card>
-                            <Card.Img variant="top" src="https://img9.doubanio.com/icon/ul188231394-13.jpg" />
+                        <Card className="card" border="dark">
+                            <Card.Header className="header">豆瓣</Card.Header>
+                            <Card.Img className="logox" variant="top" src="https://img9.doubanio.com/icon/ul188231394-13.jpg" />
                             <Card.Body>
-                                <Row>
-                                    <Col>
-                                    <Card.Img className="dblogo" src="https://cdn-icons.flaticon.com/png/512/3669/premium/3669367.png?token=exp=1637036125~hmac=a9ba7be7f21479427103022b9685e252"></Card.Img>
-                                    </Col>
+                            <Card.Link href="https://www.douban.com/people/188231394/" target={"_blank"}>
+                                <Card.Title className="title">甜甜怡</Card.Title>
+                            </Card.Link>
 
-                                    <Col>
-                                    <Row>
-                                        <Card.Link>
-                                            <Card.Title>甜甜怡</Card.Title>
-                                        </Card.Link>
-                                    </Row>
-
-                                    <Row>
-                                        <Card.Subtitle className="subtitle">Douban ID: 188231394</Card.Subtitle>
-                                    </Row>
-
-                                    <Row>
-                                        <Card.Subtitle className="subtitle">江苏 南通</Card.Subtitle>
-                                    </Row>
-
-                                    
-                                    </Col>
-
-                                </Row>
-
-
-              
+                            <Card.Subtitle className="subtitle">Douban ID: 188231394</Card.Subtitle>
+                            <Card.Subtitle className="subtitle">江苏 南通</Card.Subtitle>
+                                
                             </Card.Body>
                             <Card.Footer>
                                 加入时间：2018年12月4日
                             </Card.Footer>
                         </Card>
                     </Col>
-
                     <Col>
-                        <Card>
-                            {/* <Card.Img variant="top" src="holder.js/100px160" /> */}
+                        <Card className="card">
+                        <Card.Header className="header">微博</Card.Header>
+                            <Card.Img className="logox" variant="top" src="https://wx3.sinaimg.cn/mw2000/6de4dc19ly8grwyybvzqfj20u00u00v4.jpg" />
                             <Card.Body>
-                            <Card.Title>Card title</Card.Title>
-                            <Card.Text>
-                                This is a longer card with supporting text below as a natural
-                                lead-in to additional content. This content is a little bit longer.
-                            </Card.Text>
+
+                            <Card.Link href="https://weibo.com/n/BlueberRyan" target={"_blank"}>
+                                            <Card.Title className="title">BlueberRyan</Card.Title>
+                                        </Card.Link>
+                                        <Card.Subtitle className="subtitle">喜欢的从来不是类型 是感觉</Card.Subtitle>
+                                        <Card.Subtitle className="subtitle">江苏 南通</Card.Subtitle>
+                               
                             </Card.Body>
+                            <Card.Footer>
+                                加入时间：2010年11月1日
+                            </Card.Footer>
                         </Card>
                     </Col>
-
                     <Col>
-                        <Card>
-                            {/* <Card.Img variant="top" src="holder.js/100px160" /> */}
+                        <Card className="card">
+                        <Card.Header className="header">豆瓣</Card.Header>
+                            <Card.Img className="logox" variant="top" src="https://img9.doubanio.com/icon/ul188231394-13.jpg" />
                             <Card.Body>
-                            <Card.Title>Card title</Card.Title>
-                            <Card.Text>
-                                This is a longer card with supporting text below as a natural
-                                lead-in to additional content. This content is a little bit longer.
-                            </Card.Text>
+                            <Card.Link href="https://www.douban.com/people/188231394/" target={"_blank"}>
+                                            <Card.Title className="title">甜甜怡</Card.Title>
+                                        </Card.Link>
+
+                                        <Card.Subtitle className="subtitle">Douban ID: 188231394</Card.Subtitle>
+                                        <Card.Subtitle className="subtitle">江苏 南通</Card.Subtitle>
+                            
                             </Card.Body>
+                            <Card.Footer>
+                                加入时间：2018年12月4日
+                            </Card.Footer>
                         </Card>
                     </Col>
 
